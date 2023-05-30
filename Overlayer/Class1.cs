22d77bf2fcb397bfceda9a4cae23e8bb65027e0c
leo82380 <tkdrb062@gmail.com>
2023-05-30 10:31:09 +0900

    GUITest

--- Overlayer/Class1.cs
+++ Overlayer/Class1.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HarmonyLib;
 
 namespace Overlayer
 {
+    [HarmonyPatch(typeof(Class1), "Test")]
     class Class1
     {
+        //PreFix
+        public static void Prefix()
+        {
+            Console.WriteLine("hello world");
+            return;
+        }
     }
 }
---
+++ Overlayer/GUITest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Overlayer
+{
+    class GUITest : MonoBehaviour
+    {
+        public void OnGUI()
+        {
+            //GUI
+            GUI.Label(new Rect(10, 10, 10, 10), "Hello World");
+            //GUILayout
+            GUILayout.Label("Hello world Layout");
+            //button
+            if(GUI.Button(new Rect(10,10,10,10), "hello button"))
+            {
+                Console.WriteLine("button click!");
+            }
+
+            float val = 10;
+
+            float newVal = GUI.HorizontalSlider(new Rect(10, 10, 10, 10), val, 0, 10);
+            if(newVal != val)
+            {
+                val = newVal;
+                Console.WriteLine("slider");
+            }
+
+            //GUIStyle
+            GUIStyle textStyle = new GUIStyle();
+            textStyle.fontSize = 50;
+            textStyle.alignment = TextAnchor.UpperCenter;
+            textStyle.normal.textColor = Color.white;
+        }
+    }
+}
